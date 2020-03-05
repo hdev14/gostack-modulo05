12d7e0fb4b9aea485760b8b5a0a080096b528736
HermersonDev <ne.hermerson@gmail.com>
2020-03-05 11:09:01 -0300

    :construction: creates global styles

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -3,9 +3,4 @@ import styled from 'styled-components';
 export const Title = styled.h1`
   font-size: 2rem;
   color: green;
-
-  span {
-    display: ${props => (props.error ? 'inline-block' : 'none')};
-    color: #ff0000;
-  }
 `;
