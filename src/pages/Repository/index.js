29de9ca762119fbaae27a90446ff1611b61de4a9
HermersonDev <ne.hermerson@gmail.com>
2020-03-06 11:19:42 -0300

    :construction: transform the repository function component in class component

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Repository({ match }) {
-  return <h1>{decodeURIComponent(match.params.repo)}</h1>;
+import api from '../../services/api';
+
+export default class Repository extends React.Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repo: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
+  state = {
+    repo: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const repoName = decodeURIComponent(match.params.repo);
+
+    const [repo, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repo: repo.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repo, issues, loading } = this.state;
+    return <h1>Repositório</h1>;
+  }
 }
